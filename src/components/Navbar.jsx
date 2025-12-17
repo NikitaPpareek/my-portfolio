84ea127df63d20232d384dd8760e684bb44ce6f1
NikitaPpareek <nikitapareek1809@gmail.com>
2025-12-17 22:04:14 +0530

    Finalize full-width fixed navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,23 @@
-const Navbar =()=>{
-    return 
-}
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+
+        <h2 className="navbar-logo">Nikita</h2>
+
+        <ul className="navbar-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
